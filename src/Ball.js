b16d25b53ca9417922df7bc79176fc04f1f04bf7
Miguel-Venegas <venegas.a.miguel@gmail.com>
2021-03-02 06:19:24 -0800

    Learning about controlling state and props by creating a Lottery Numbers Generator

---
+++ src/Ball.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Ball.css';
+
+class Ball extends Component {
+    
+    render() {
+        const {num} = this.props;
+        return (
+            <div className="Ball">
+                {num}
+            </div>
+        );
+    }
+}
+
+export default Ball;
